21715da02e5b88f08e0e4043ef2b669f9dd27bfe
Kevin Conteron <kconteron6824@uta.edu.ec>
2023-04-25 19:16:23 -0500

    cambios

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ export const dbSettings = {
     user: "Diegol_SQLLogin_1",
     password: "cr9dxs9job",
     server: "MinimarketGJ.mssql.somee.com",
-    database:"GRUPOJ",
+    database:"minimarketgj",
     options: {
       encrypt: true, // for azure
       trustServerCertificate: true, // change to true for local dev / self-signed certs
